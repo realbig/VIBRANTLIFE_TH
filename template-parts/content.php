61352947ae394c90498c1eb529ce9b1cde375216
joelworsham <joel.worsham@gmail.com>
2018-07-02 09:08:34 -0400

    Fix double title on single.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,29 +11,33 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-	<?php
-		if ( is_single() ) {
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		} else {
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		}
-	?>
-		<?php foundationpress_entry_meta(); ?>
-	</header>
-	<div class="entry-content">
+	<?php if ( ! has_post_thumbnail() ) : ?>
+        <header>
+			<?php
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+			?>
+        </header>
+	<?php endif; ?>
+	<?php foundationpress_entry_meta(); ?>
+
+    <div class="entry-content">
 		<?php the_content(); ?>
 		<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-	</div>
-	<footer>
+    </div>
+    <footer>
 		<?php
-			wp_link_pages(
-				array(
-					'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-					'after'  => '</p></nav>',
-				)
-			);
+		wp_link_pages(
+			array(
+				'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
+				'after'  => '</p></nav>',
+			)
+		);
 		?>
-		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
-	</footer>
+		<?php $tag = get_the_tags();
+		if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
+    </footer>
 </article>
