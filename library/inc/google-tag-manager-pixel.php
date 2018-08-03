55103b7a8ddff3eaa318466dd6cdf6999a487d63
joelworsham <joel.worsham@gmail.com>
2018-08-03 15:51:36 -0400

    Add GTM pixel & v1.0.1

---
+++ library/inc/google-tag-manager-pixel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Google Tag Manager Pixel code. This will be inserted into the header of the site. Please modify code in between the
+ * denoted area.
+ *
+ * @since {{VERSION}}
+ */
+
+defined( 'ABSPATH' ) || die();
+
+// @formatter:off
+?>
+
+<?php // Edit below here ?>
+
+<!-- Google Tag Manager Pixel -->
+<script async src='https://tag.simpli.fi/sifitag/494a1000-0941-0136-44df-067f653fa718'></script>
+<!-- End Google Tag Manager Pixel -->
+
+<?php // Edit above here ?>
--- library/third-party.php
+++ library/third-party.php
@@ -10,6 +10,7 @@ defined( 'ABSPATH' ) || die();
 // Google Tag Manager
 add_action( 'wp_head', 'vibrantlife_google_tag_manager_a_code', 99999999 );
 add_action( 'vibrantlife_body_open', 'vibrantlife_google_tag_manager_b_code', 1 );
+add_action( 'wp_head', 'vibrantlife_google_tag_manager_pixel_code' );
 
 /**
  * Outputs the Google Tag Manager (A) code.
@@ -21,6 +22,7 @@ function vibrantlife_google_tag_manager_a_code() {
 
 	include_once 'inc/google-tag-manager-a.php';
 }
+
 /**
  * Outputs the Google Tag Manager (B) code.
  *
@@ -30,4 +32,15 @@ function vibrantlife_google_tag_manager_a_code() {
 function vibrantlife_google_tag_manager_b_code() {
 
 	include_once 'inc/google-tag-manager-b.php';
+}
+
+/**
+ * Outputs the Google Tag Manager Pixel code.
+ *
+ * @since {{VERSION}}
+ * @access private
+ */
+function vibrantlife_google_tag_manager_pixel_code() {
+
+	include_once 'inc/google-tag-manager-pixel.php';
 }
